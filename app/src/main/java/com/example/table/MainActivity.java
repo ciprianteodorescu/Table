311f4c5849447c1ffc0bf7d27ab07a3a057357e1
ciprianteodorescu <crptn1000@gmail.com>
2021-04-06 00:13:25 +0300

    layout tabla initial

--- app/src/main/java/com/example/table/MainActivity.java
+++ app/src/main/java/com/example/table/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.table;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView im1, im2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        im1 = new ImageView(this);
+        im2 = new ImageView(this);
+
+        im1 = (ImageView) findViewById(R.id.imageView);
+        im2 = (ImageView) findViewById(R.id.imageView1);
     }
 }
